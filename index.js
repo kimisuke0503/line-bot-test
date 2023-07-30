3655d6753d4c500f5bc5dc3be096281c86a24555
kimisuke <kimisuke0503@gmail.com>
2023-07-30 11:59:24 +0900

    fix

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-"use strict";
-var _a, _b;
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var bot_sdk_1 = require("@line/bot-sdk");
-var config = {
-    channelSecret: (_a = process.env.CHANNEL_SECRET) !== null && _a !== void 0 ? _a : "",
-    channelAccessToken: (_b = process.env.CHANNEL_ACCESS_TOKEN) !== null && _b !== void 0 ? _b : "",
+import express from "express";
+import { Client, middleware } from "@line/bot-sdk";
+
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
-var client = new bot_sdk_1.Client(config);
-var PORT = process.env.PORT || 3000;
-var app = (0, express_1.default)();
-app.post("/", (0, bot_sdk_1.middleware)(config), function (req, res) {
-    Promise.all(req.body.events.map(handleEvent)).then(function (result) {
-        return res.json(result);
-    });
+
+const client = new Client(config);
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.post("/", middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result),
+  );
 });
+
 app.listen(PORT);
-var handleEvent = function (event) {
-    if (event.type !== "message" || event.message.type !== "text") {
-        return Promise.resolve(null);
-    }
-    return client.replyMessage(event.replyToken, {
-        type: "text",
-        text: event.message.text,
-    });
+
+const handleEvent = (event) => {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
+  });
 };
